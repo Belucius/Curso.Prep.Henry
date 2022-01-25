2635296c59a0a31095c6d2ad631b413f8cf72da2
Belen <beluudelcerro@gmail.com>
2022-01-25 16:38:08 -0300

    02-JS-I/homework/instrucciones.js

---
+++ 02-JS-I/homework/instrucciones.js
@@ -0,0 +1,11 @@
+const { parseGetRemotes } = require("simple-git/src/lib/responses/GetRemoteSummary")
+
+Variables: Las variables son recursos que guardamos en una caja para utilizarlos cuando sea necesario. 
+
+Instrumentos de cuerda: Son aquellos que nos indican con que informacion vamos a manejarnos, los valores que toman y la operaciones que pueden realizarse.
+
+Funciones: Estos son objetos que utilizaremos para realizar nuestra tarea y, como todo objeto esta compuesto por partes 
+
+Declaraciones: Nos ayudan a recordar formulas. 
+
+Valores booleanos: Indican si es verdadero o falso lo que estamos realizando.
