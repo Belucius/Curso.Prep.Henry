3ceb40197e0cc28f4f9861d101aa03bfe146e344
Belen <beluudelcerro@gmail.com>
2022-01-28 17:16:04 -0300

    03-JS-II/homework/homework.js

---
+++ 03-JS-II/homework/instruccion.js
@@ -0,0 +1,3 @@
+for: los bucles for tienen una receta magica, con tres ingredientes que resulta increible. Esta misma receta permite regresar siempre al mismo lugar .
+
+operadores logicos: son recetas que siempre devuelven el valor verdadero o falso segun corresponda. 
