4cce57fba5f6c75851048670acb146cbe7f7a763
Belen <beluudelcerro@gmail.com>
2022-02-07 10:06:34 -0300

    05-JS-IV/homework/instrucciones.js

---
+++ 05-JS-IV/homework/instrucciones.js
@@ -0,0 +1,9 @@
+* Objetos: son aquellos que contienen mucha informacion en su interior entre llaves. 
+
+	* Propiedades: es el valor entre un nombre y un valor. 
+
+	* Métodos: son una combinaciones guaradas en un objeto.
+
+	* Bucle `for…in`: los bucles nos permiten iterar entre la clave y valor del objeto.
+
+	* Notación de puntos vs notación de corchetes:
